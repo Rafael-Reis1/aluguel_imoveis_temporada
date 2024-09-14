cc69a7a7862b1656bdfe802262963183670e15ab
Rafa3lR <rafaelreis.12@hotmail.com>
2024-09-14 03:01:10 -0300

    Implementa login, guard e localStrategy

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { ImoveisModule } from './modules/imoveis/imoveis.module';
 import { ReservasModule } from './modules/reservas/reservas.module';
 import { UsuariosModule } from './modules/usuarios/usuarios.module';
-import { AuthModule } from './auth/auth.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [ImoveisModule, ReservasModule, UsuariosModule, AuthModule],
