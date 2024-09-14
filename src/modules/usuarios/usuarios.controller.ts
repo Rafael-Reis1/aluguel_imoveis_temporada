5329d56b9274a19e42e3dbe39ccb0169f0bbbd51
Rafa3lR <rafaelreis.12@hotmail.com>
2024-09-14 19:42:41 -0300

    Implementa usuario atual

--- src/modules/usuarios/usuarios.controller.ts
+++ src/modules/usuarios/usuarios.controller.ts
@@ -1,6 +1,9 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateUsuarioDto } from './dto/create-usuario.dto';
 import { UsuariosService } from './usuarios.service';
+import { IsPublic } from '../auth/decorators/is-public.decorator';
+import { UsuarioAtual } from '../auth/decorators/usuario-atual.decorator';
+import { Usuario } from '@prisma/client';
 
 @Controller('usuarios')
 export class UsuariosController {
@@ -9,5 +12,10 @@ export class UsuariosController {
   @Post()
   create(@Body() createUsuarioDto: CreateUsuarioDto) {
     return this.usuariosService.create(createUsuarioDto);
+  }
+
+  @Get('me')
+  getMe(@UsuarioAtual() usuario: Usuario) {
+    return usuario;
   }
 }
